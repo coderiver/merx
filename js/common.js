d92235b376587dbe0a66e3e992c66821c0fbf6bd
Artur Moroz <morozarturv@gmail.com>
2013-11-26 17:29:37 +0200

    added some pages

--- js/common.js
+++ js/common.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 
 //menu
 function menu() {
-	var el = $('.js-main');
-	var item = el.find('.menu__item-in');
+	var el = $('.js-sl-category');
+	var item = $('.menu__item-in');
 	item.hoverIntent(nav_1, nav_2);
 	function nav_1() {
 		$(this).addClass('is-hover').next().fadeIn();
 		var item = $(this).attr('data-item');
-		el.find('.main__sl-item').removeClass('is-active');
+		el.find('.sl-category__item').removeClass('is-active');
 		$('.'+ item).addClass('is-active');
 	};
 	function nav_2() {
